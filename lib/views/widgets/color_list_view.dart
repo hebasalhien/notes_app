992c30713e1af5d0c1d1768a24c14d7b4169a137
hebasalhien <hebasalhien62@gmail.com>
2025-05-19 08:03:04 +0300

    create colors list view

---
+++ lib/views/widgets/color_list_view.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+class ColorItem extends StatelessWidget {
+  const ColorItem({super.key, required this.picked, required this.color});
+  final bool picked;
+  final Color color;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8),
+      child:
+          picked
+              ? CircleAvatar(
+                backgroundColor: Colors.yellow.shade300,
+                radius: 40,
+                child: CircleAvatar(backgroundColor: color, radius: 36),
+              )
+              : CircleAvatar(backgroundColor: color, radius: 38),
+    );
+  }
+}
+
+class ColorListView extends StatefulWidget {
+  const ColorListView({super.key});
+
+  @override
+  State<ColorListView> createState() => _ColorListViewState();
+}
+
+class _ColorListViewState extends State<ColorListView> {
+  int currentIndex = 0;
+  List<Color> colors = const [
+    Color(0xff3eb489),
+    Color(0xff267055),
+    Color(0xff2F5233),
+    Color(0xffb1d8b7),
+    Color(0xff167d7f),
+    Color(0xff29a0b1),
+    Color(0xff8fdde7),
+    Color(0xffb6e5d8),
+    Color(0xfffbe5c8),
+    Color(0xffffc2c7),
+    Color(0xffffaebc),
+    Color(0xfffd49a0),
+    Color(0xffa16ae8),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 80,
+      child: ListView.builder(
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            onTap: () {
+              currentIndex = index;
+              setState(() {});
+            },
+            child: ColorItem(
+              picked: currentIndex == index,
+              color: colors[index],
+            ),
+          );
+        },
+        itemCount: colors.length,
+        scrollDirection: Axis.horizontal,
+      ),
+    );
+  }
+}
